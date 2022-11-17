d2d897a8995f7e092f1799baeab7e8ea194b57e1
iriswk <irisalarcon@wuqukawoq.org>
2022-11-17 08:41:22 -0600

    App

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,25 @@
-import {initializeApp} from 'firebase/app'
-import {getFirestore, collection, addDoc} from 'firebase/firestore'
-import {getAuth, signInWithEmailAndPassword,
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import {
+    getAuth,
+    signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
-    sendPasswordResetEmail, signOut} from 'firebase/auth'
-
+    sendPasswordResetEmail,
+    signOut,
+} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAzY1MZsPeobx1-BwF6eaBcaHh811D_-3E",
-    authDomain: "practica-final-92bb5.firebaseapp.com",
-    projectId: "practica-final-92bb5",
-    storageBucket: "practica-final-92bb5.appspot.com",
-    messagingSenderId: "1059769625087",
-    appId: "1:1059769625087:web:8a130aa7a41ab1badc1fca"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app)
-
-
+const db = getFirestore(app);
 
 const logInWithEmailAndPassword = async (email, password) => {
     try {
@@ -59,10 +60,11 @@ const logout = () => {
     signOut(auth);
 };
 
-
-export {auth,
+export {
+    auth,
     db,
     logInWithEmailAndPassword,
     registerWithEmailAndPassword,
     sendPasswordReset,
-    logout,}
+    logout,
+};
